13c60c460267184c28c987f60f8a2e864269a2c4
leskof <yavuzbuldag@gmail.com>
2023-02-09 14:38:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Java Loop Project 8 [patika.dev](https://app.patika.dev/courses/java101/pratik-yildiz-ucgen)
+___
+## Description 
+
+This project create a "Diamond" with "*". Diamond size changing with your input size.
+
+___
+_This project created by [@leskof](https://github.com/leskof)_
---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Shape size: ");
+        int num = input.nextInt();
+
+        for (int i = 0; i < num - 1; i++) {
+            for (int j = 0; j < (num - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for(int i = num - 1; i >= 0; i--) {
+            for (int j = 1; j <= (num - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
